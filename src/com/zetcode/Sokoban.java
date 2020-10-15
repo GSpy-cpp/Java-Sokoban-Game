e4cf3071b31ab4aaefccbeccebcd3406d34583c9
GSpy-cpp <55821668+GSpy-cpp@users.noreply.github.com>
2020-10-16 06:45:03 +0900

    feature : changePanel() 메소드 구현
    changePanel() 메소드의 인터페이스를 유지하면서 기존의 문제점을 해결함.
    viewController는 안써도 될거같음.

--- src/com/zetcode/Sokoban.java
+++ src/com/zetcode/Sokoban.java
@@ -8,13 +8,14 @@ public class Sokoban extends JFrame {// main Ŭ����
 	final static int FRAME_WIDTH = 500;
 	final static int FRAME_HIGHT = 500;
 
-	private final int    OFFSET              = 30;
-	private final String LOGINPANEl_NAME     = "loginPanel";
+	private final int    OFFSET               = 30;
+	private final String LOGINPANEl_NAME      = "loginPanel";
 	private final String INTROPANEL_NAME      = "introPanel";
 	private final String GAMEPANEL_NAME       = "gamePanel";
 	private final String BASICPANEL_NAME      = "basicPanel";
 	private final String HIDEPANEL_NAME       = "hidePanel";
 	private final String PUTINORDERPANEL_NAME = "putinorderPanel";
+	private final String GAUGEPANEL_NAME      = "gaugePanel";
 
 	private CardLayout cards;
 
@@ -27,7 +28,28 @@ public class Sokoban extends JFrame {// main Ŭ����
 	// JFrame에 표시되는 Panel을 바꿀 때 사용
 	// 사용예 : frame.changePanel(frame.getLobbyPanelName());
 	public void changePanel(String name) {
-		this.cards.show(this.getContentPane(), name);
+		getContentPane().removeAll();
+
+		if (name.equals(INTROPANEL_NAME)) {
+			getContentPane().add(new IntroPanel(this));
+		}
+		if (name.equals(GAMEPANEL_NAME)) {
+			getContentPane().add(new GamePanel(this));
+		}
+		if (name.equals(BASICPANEL_NAME)) {
+			getContentPane().add(new BasicPanel(this));
+		}
+		if (name.equals(HIDEPANEL_NAME)) {
+			getContentPane().add(new HideSightPanel(this));
+		}
+		if (name.equals(PUTINORDERPANEL_NAME)) {
+			getContentPane().add(new PutInOrderPanel(this));
+		}
+		if (name.equals(GAUGEPANEL_NAME)) {
+			getContentPane().add(new GaugePanel(this));
+		}
+
+		repaint();
 	}
 
 	private void initUI() {
@@ -39,11 +61,11 @@ public class Sokoban extends JFrame {// main Ŭ����
 
 		// 사용되는 panel을 모두 add 해야 함.
 		getContentPane().add(this.getLoginPanelName(), new LoginPanel(this));
-		getContentPane().add(this.getIntroPanelName(), new IntroPanel(this));
-		getContentPane().add(this.getGamePanelName(), new GamePanel(this));
-		getContentPane().add(this.getBasicPanelName(), new BasicPanel(this));
-		getContentPane().add(this.getHidePanelName(), new HidePanel(this));
-		getContentPane().add(this.getPutInOrderPanelName(), new PutInOrderPanel(this));
+//		getContentPane().add(this.getIntroPanelName(), new IntroPanel(this));
+//		getContentPane().add(this.getGamePanelName(), new GamePanel(this));
+//		getContentPane().add(this.getBasicPanelName(), new BasicPanel(this));
+//		getContentPane().add(this.getHidePanelName(), new HidePanel(this));
+//		getContentPane().add(this.getPutInOrderPanelName(), new PutInOrderPanel(this));
 
 	}
 
