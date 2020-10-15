40732ed767a2ed7f2b6332c09e7376f1794fc59d
GSpy-cpp <55821668+GSpy-cpp@users.noreply.github.com>
2020-10-16 01:29:05 +0900

    수정 : getH -> getHidePanelName()

--- src/com/zetcode/GamePanel.java
+++ src/com/zetcode/GamePanel.java
@@ -67,7 +67,7 @@ public class GamePanel extends JPanel {
 	}
 
 	public void hideStart() {
-		sokoban.changePanel(sokoban.getH);
+		sokoban.changePanel(sokoban.getHidePanelName());
 	}
 //
 //	public void gaugeStart() {
